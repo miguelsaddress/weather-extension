5c02f4e7e2236740c45c00b436e5bbe0d676a5c2
Rodrigo Alfaro <rodrigo@rentalia.com>
2017-02-10 06:11:49 +0100

    Changed to ES6 backtick template. More efficient way.

--- ext/js/popUp.js
+++ ext/js/popUp.js
@@ -7,33 +7,69 @@ weather.getForecast(function(forecast) {
 
     let current = forecast.getFutureDay(0);
 
-    // TODO this is sloooooooooow, change it by placeholders or templates
+    let currentData = {
+        tempAvg: Math.round(current.tempAvg),
+        tempMin: Math.round(current.tempMin),
+        tempMax: Math.round(current.tempMax),
+        icon: '/icons/' + current.icon.substring(0,2) + '.svg'
+    };
 
-
-    document.querySelector('.forecast-temp').innerHTML = Math.round(current.tempAvg) + '°C';
-    document.querySelector('.forecast-min > .main-grown').innerHTML = Math.round(current.tempMin) + '°C';
-    document.querySelector('.forecast-max > .main-grown').innerHTML = Math.round(current.tempMax) + '°C';
-    document.querySelector('.main-icon > img').src = '/icons/' + current.icon.substring(0,2) + '.svg';
-
-
-    let weekName = document.querySelectorAll('.card-header');
-    let temp = document.querySelectorAll('.card-temp');
-    let min = document.querySelectorAll('.card-min > .card-grown');
-    let max = document.querySelectorAll('.card-max > .card-grown');
-    let icon = document.querySelectorAll('.card-image > img');
-
+    let forecastData = [];
 
     for (let i = 0; i < 3; i++) {
         let day = forecast.getFutureDay(i + 1);
 
-        weekName[i].innerHTML = day.dayName.toUpperCase();
-        temp[i].innerHTML = Math.round(day.tempAvg) + '°';
-        min[i].innerHTML = Math.round(day.tempMin) + '°';
-        max[i].innerHTML = Math.round(day.tempMax) + '°';
-        icon[i].src = '/icons/' + day.icon.substring(0,2) + '.svg';
-
+        forecastData[i] = {
+            dayName: day.dayName.toUpperCase(),
+            tempMin: Math.round(day.tempMin),
+            tempMax: Math.round(day.tempMax),
+            tempAvg: Math.round(day.tempAvg),
+            icon: '/icons/' + day.icon.substring(0,2) + '.svg'
+        };
     }
 
-
+    template({currentData, forecastData});
 });
 
+
+
+function template(content) {
+
+     let main = `
+        <div class="title">Madrid hoy</div>
+        <div id="main">
+                <div class="main-temp">
+                    <div class="forecast-temp">${content.currentData.tempAvg}°C</div>
+                    <div class="forecast-min">MÍN <span class="main-grown"></span>${content.currentData.tempMin}°C</div>
+                    <div class="forecast-max">MÁX <span class="main-grown"></span>${content.currentData.tempMax}°C</div>
+                </div>
+                <div class="main-icon">
+                    <img src="${content.currentData.icon}" alt="forecast today">
+                </div>
+        </div>
+        `;
+
+    // card number independent
+    let cards = '';
+    content.forecastData.forEach(function (forecastDay) {
+        cards += `
+            <div class="card">
+                <div class="card-header">${forecastDay.dayName}</div>
+                <div class="card-image"><img src="${forecastDay.icon}" alt="" ></div>
+                <div class="card-inside">
+                    <div class="card-temp">${forecastDay.tempAvg}°</div>
+                    <div class="card-min">MÍN <span class="card-grown">${forecastDay.tempMin}°</span></div>
+                    <div class="card-max">MÁX <span class="card-grown">${forecastDay.tempMax}°</span></div>
+                </div>
+            </div>
+         `;
+    });
+
+    let deck = `
+        <div id="deck">
+            ${cards}
+        </div>
+    `;
+
+    document.getElementById("container").innerHTML = main + deck;
+}
